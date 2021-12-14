b20d0dd023274dffd0f4e67c0a45fe3d2ec1a92d
Sonu Paswan <sonupas2806@gmail.com>
2021-12-14 11:43:33 +0530

    mdf dec to binary

--- c++/decimal_to_binary.cpp
+++ c++/decimal_to_binary.cpp
@@ -1,18 +1,31 @@
-//C++ program to convert decimal number to binary number
+// C++ program to convert decimal number to binary number
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
+    vector<int> myVec;
     int n, i, bits, result;
     cout << "enter any decimal number : ";
     cin >> n;
     cout << "binary number of " << n << " is\n";
-
+    bool flag=true;
     for (i = 15; i >= 0; i--)
     {
         bits = 1 << i;
         result = n & bits;
-        result == 0 ? cout << "0" : cout << "1";
+        // result == 0 ? cout << "0" : cout << "1";
+        if (result != 0 )
+        {   flag=false;
+            myVec.push_back(1);
+        }
+        if(result==0 && flag==false){
+            myVec.push_back(0);
+        }
+    }
+    for (int i = 0; i < myVec.size(); i++)
+    {
+        cout << myVec[i];
     }
 
     return 0;
